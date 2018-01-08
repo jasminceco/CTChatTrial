97691490e94559b627f79db64095f0e9358a8678
Jasmin Ceco <jasminceco@jasmincecomac.local>
2018-01-08 16:23:12 +0100

    podspec version 0.4.0

--- Example/CTChatTrial2/ViewController.swift
+++ Example/CTChatTrial2/ViewController.swift
@@ -27,9 +27,9 @@ class ViewController: UIViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(openChatwithNewUser(_:)), name: .openNewChat, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(uploadToFirebaseStorageUsingImage(_:)), name: .sendImage, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(uploadToFirebaseStorageUsingVideo(_:)), name: .sendVideo, object: nil)
-        
         NotificationCenter.default.addObserver(self, selector: #selector(handleLoging(_:)), name: .hangleLogin, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(handleRegister(_:)), name: .hangleRegister, object: nil)
+        //MARK: Configure chat :)
         Configuration.shouldShowMessagesHeader = true
         Configuration.ChatBubbleFromColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
     }
